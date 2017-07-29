c0df428df0e442d51834702d790cf1e9f80e4ce5
Valentyn Fomenko <fomenko.vn@gmail.com>
2017-07-29 19:51:43 +0300

    add comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp 					 = require('gulp'), // Подключаем Gulp
-		browserSync    = require('browser-sync');
+		browserSync    = require('browser-sync'); // ... browser-sync
 
 gulp.task('browser-sync', function() { // Создаем таск browser-sync
 	browserSync({ // Выполняем browserSync
 		server: { // Определяем параметры сервера
-			baseDir: '02 - JS and CSS Clock', // Директория для сервера - app
-			index: "index-VAL.html"
+			baseDir: '02 - JS and CSS Clock', // Директория для сервера - для каждой задачи соответствующая
+			index: "index-VAL.html" // кастомный индекс-файл. Во всех задачах одинаковый.
 		},
 		notify: false,// Отключаем уведомления
 		// tunnel: true,
@@ -14,8 +14,8 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('watch', ['browser-sync'], function() {
-	gulp.watch('**/*.css', browserSync.reload); // Наблюдение за css файлами в папке sass
-	gulp.watch('**/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch('**/*.css', browserSync.reload); // Наблюдение за css файлами в папках задач
+	gulp.watch('**/*.html', browserSync.reload); // Наблюдение за HTML файлами в папках задач
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch']); // дефолтный таск галпа - watch
